3cef451903ac80803c776c154b5535144dd09b31
Guilherme <118012347+GuilhermeCN-web@users.noreply.github.com>
2026-03-11 16:56:52 -0300

    :sparkles: feat: CourseService.java changed

--- src/main/java/com/fatec/horario/domain/services/CourseService.java
+++ src/main/java/com/fatec/horario/domain/services/CourseService.java
@@ -1,8 +1,11 @@
 package com.fatec.horario.domain.services;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.fatec.horario.domain.entities.Course;
 import com.fatec.horario.dto.Course.CourseRequest;
 import com.fatec.horario.dto.Course.CourseResponse;
@@ -13,11 +16,11 @@ import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CourseService {
+
     @Autowired
     private CourseRepository repository;
 
-   
-
+    @Transactional(readOnly = true)
     public List<CourseResponse> getAll() {
         return repository.findAll()
                 .stream()
@@ -25,6 +28,7 @@ public class CourseService {
                 .toList();
     }
 
+    @Transactional(readOnly = true)
     public CourseResponse getById(Long id) {
         Course course = repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Course not found with id: " + id));
@@ -32,28 +36,36 @@ public class CourseService {
         return CourseMapper.toResponse(course);
     }
 
+    @Transactional
     public CourseResponse create(CourseRequest request) {
         Course course = CourseMapper.toEntity(request);
 
-       
         course = repository.save(course);
+
         return CourseMapper.toResponse(course);
     }
 
+    @Transactional
     public CourseResponse update(Long id, CourseRequest request) {
+
         Course course = repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Course not found with id: " + id));
+
         course.setName(request.name());
         course.setDescription(request.description());
 
         course = repository.save(course);
+
         return CourseMapper.toResponse(course);
     }
 
+    @Transactional
     public void delete(Long id) {
+
         if (!repository.existsById(id)) {
             throw new EntityNotFoundException("Course not found with id: " + id);
         }
+
         repository.deleteById(id);
     }
 }
