3dd11e21dc5431f8d625088c77fbf269ccf8ef39
felipePaterni <fspaterni.chaves@gmail.com>
2026-04-15 14:48:40 -0300

    ✨ feat: implementa relacionamento com Alocação na entidade Turma

--- src/main/java/com/fatec/horario/domain/entities/Turma.java
+++ src/main/java/com/fatec/horario/domain/entities/Turma.java
@@ -2,7 +2,6 @@ package com.fatec.horario.domain.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,13 +36,8 @@ public class Turma {
     @JoinColumn(name = "id_curso", nullable = false)
     private Curso curso;
 
-     /**
-     * Implementar relacionamento com Alocação no futuro
-     * 
-     * @OneToMany(mappedBy = "turma")
-     * private List<Alocacao> alocacoes;
-     * 
-     */
+    @OneToMany(mappedBy = "turma")
+    private List<Alocacao> alocacoes;
 
     public Turma() {
     }
@@ -105,7 +99,6 @@ public class Turma {
         this.curso = curso;
     }
 
-    /* 
     public List<Alocacao> getAlocacoes() {
         return alocacoes;
     }
@@ -113,8 +106,7 @@ public class Turma {
     public void setAlocacoes(List<Alocacao> alocacoes) {
         this.alocacoes = alocacoes;
     }
-    */
-   
+
     @Override
     public int hashCode() {
         final int prime = 31;
