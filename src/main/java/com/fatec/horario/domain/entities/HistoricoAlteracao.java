e0d62a2b1c89780e7681de49ce68ce79fb2b750e
FelipePaterni <fspaterni.chaves@gmail.com>
2026-04-16 18:09:17 -0300

    🚧 feat : crud

---
+++ src/main/java/com/fatec/horario/domain/entities/HistoricoAlteracao.java
@@ -0,0 +1,142 @@
+package com.fatec.horario.domain.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class HistoricoAlteracao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_historico_alteracao")
+    private Long id;
+
+    @Column(name = "data_alteracao", nullable = false)
+    private LocalDate dataAlteracao;
+
+    @Column(nullable = false)
+    private String justificativa;
+
+    @Column(name = "campo_alterado", nullable = false)
+    private String campoAlterado;
+
+    @Column(name = "valor_antigo")
+    private String valorAntigo;
+
+    @Column(name = "valor_novo", nullable = false)
+    private String valorNovo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_alocacao", nullable = false)
+    private Alocacao alocacao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    public HistoricoAlteracao() {
+    }
+
+    public HistoricoAlteracao(LocalDate dataAlteracao, String justificativa, String campoAlterado, String valorAntigo,
+            String valorNovo) {
+        this.dataAlteracao = dataAlteracao;
+        this.justificativa = justificativa;
+        this.campoAlterado = campoAlterado;
+        this.valorAntigo = valorAntigo;
+        this.valorNovo = valorNovo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataAlteracao() {
+        return dataAlteracao;
+    }
+
+    public void setDataAlteracao(LocalDate dataAlteracao) {
+        this.dataAlteracao = dataAlteracao;
+    }
+
+    public String getJustificativa() {
+        return justificativa;
+    }
+
+    public void setJustificativa(String justificativa) {
+        this.justificativa = justificativa;
+    }
+
+    public String getCampoAlterado() {
+        return campoAlterado;
+    }
+
+    public void setCampoAlterado(String campoAlterado) {
+        this.campoAlterado = campoAlterado;
+    }
+
+    public String getValorAntigo() {
+        return valorAntigo;
+    }
+
+    public void setValorAntigo(String valorAntigo) {
+        this.valorAntigo = valorAntigo;
+    }
+
+    public String getValorNovo() {
+        return valorNovo;
+    }
+
+    public void setValorNovo(String valorNovo) {
+        this.valorNovo = valorNovo;
+    }
+
+    public Alocacao getAlocacao() {
+        return alocacao;
+    }
+
+    public void setAlocacao(Alocacao alocacao) {
+        this.alocacao = alocacao;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        HistoricoAlteracao other = (HistoricoAlteracao) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
