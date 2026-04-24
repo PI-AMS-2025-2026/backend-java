57f84dc240a35ef351b24a8754882571928639fe
Felipe <fspaterni.chaves@gmail.com>
2026-04-24 16:52:18 -0300

    Update ProfessorDisciplinaRepository.java

--- src/main/java/com/fatec/horario/infrastructure/repositories/ProfessorDisciplinaRepository.java
+++ src/main/java/com/fatec/horario/infrastructure/repositories/ProfessorDisciplinaRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProfessorDisciplinaRepository extends JpaRepository<ProfessorDisciplina, Long> {
 
-    boolean existsByUsuarioIdUsuarioAndDisciplinaId(Long usuarioId, Long disciplinaId);
+    boolean existsByUsuarioIdAndDisciplinaId(Long usuarioId, Long disciplinaId);
 
     Page<ProfessorDisciplina> findByUsuarioId(Long usuarioId, Pageable pageable);
 
